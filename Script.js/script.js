a5c0d06f6182f849965f9c4743b85c2e77c5946d
Mateus <mateusrochamr133@gmail.com>
2023-11-22 17:28:46 -0300

    LogicaJavaScript

---
+++ Script.js/script.js
@@ -0,0 +1,108 @@
+/*let num1 = prompt("Digite um número:");
+
+let num2 = prompt("Digite outro número:");
+
+let op = prompt("Digite uma operação matematica:");
+
+let soma;
+
+if(op == "+"){
+    soma = Number(num1) + Number(num2);
+}else if(op == "-"){
+    soma = Number(num1) - Number(num2);
+}else if(op =="*"){
+    soma = Number(num1) * Number(num2);
+}else if(op =="/"){
+    soma = Number(num1) / Number(num2);
+}else{
+    alert('Informe uma op válida');
+}
+
+==>Comparaçao
+
+alert('A soma da op é ' +soma);
+*/
+
+/*let a = prompt('Digite um numero')
+
+let b = prompt('Digite outro numero')
+
+let c = prompt('Digite mais um numero')
+
+let d = prompt('Digite mais um outro numero')
+
+let soma;
+
+soma = (Number(a) + Number(b) + Number(c) + Number(d))/4;*/
+
+
+/*if( op =="+"){
+    soma = Number(a) + Number(b)
+}else if( == "-"){
+    
+}
+if( op =="/"){
+    soma = Number(c) + Number(d)
+}else if ( == "*"){
+    
+}*/
+
+/*alert('A soma da op é' + soma)*/
+
+
+/*let a = prompt('Quantos kms percorreu o seu carro?')
+
+let b = prompt('Quantos dias voce rodou?')
+
+let soma = ( Number(a)*0.20) + ( Number(b)*90)
+
+alert('Você precisa pagar ' + soma )*/
+
+/*let a = prompt('Quantos kms voce deseja percorrer?')
+
+let km = 0.50
+
+let m = 0.45
+
+let soma
+
+
+if(a <= 200){
+    soma = Number(a) * Number(km)
+}else if(a > 200){
+    soma = Number(a) * Number(m)
+}
+
+alert('A soma é ' + soma)*/
+
+
+/*let a =   prompt('Digite uma nota')
+
+let b =  prompt('Digite outra nota')
+
+let soma
+
+soma = (Number(a) + Number(b) ) /2
+
+if( soma <=4.9){
+    alert('Voce foi reprovado')
+}
+else if( soma >=5 && soma <=6.9)
+{
+    alert('Voce esta em recuperaçao')}
+else{ 
+    alert('Voce esta aprovado')
+}
+
+
+alert('A sua nota é de ' +soma)*/
+
+
+
+let a = prompt('Digite seu nome')
+
+let b = prompt('Digite seu salario')
+
+let c = prompt('Quantos anos você trabalha?')
+
+let d = prompt('')
